d9acaa4a4d6fb889781da6d394dede3d7d60c9e5
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-15 18:43:34 +0000

    Added to dataloader

---
+++ src/main/java/com/example/cakeShop/components/DataLoader.java
@@ -0,0 +1,41 @@
+package com.example.cakeShop.components;
+
+import com.example.cakeShop.models.Cake;
+import com.example.cakeShop.models.CakeShop;
+import com.example.cakeShop.repositories.CakeRepository;
+import com.example.cakeShop.repositories.CakeShopRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader {
+
+    @Autowired
+    CakeRepository cakeRepository;
+
+    @Autowired
+    CakeShopRepository cakeShopRepository;
+
+    public DataLoader(){}
+
+    public void run(ApplicationArguments args){
+        CakeShop patisserieCakes = new CakeShop("Patisserie Cakes");
+        cakeShopRepository.save(patisserieCakes);
+
+        Cake redVelvetCake = new Cake("Red Velvet Cake", 6.00, patisserieCakes);
+        cakeRepository.save(redVelvetCake);
+
+        Cake oreoCheeseCake = new Cake("Oreo Cheesecake", 6.00, patisserieCakes);
+        cakeRepository.save(oreoCheeseCake);
+
+        Cake victoriaSponge = new Cake("Victoria Sponge Cake", 6.00, patisserieCakes);
+        cakeRepository.save(victoriaSponge);
+
+        Cake blackForestCake = new Cake("Black Forest Cake", 5.90, patisserieCakes);
+        cakeRepository.save(blackForestCake);
+
+        Cake tiramisu = new Cake("Tiramisu", 5.00, patisserieCakes);
+        cakeRepository.save(tiramisu);
+    }
+}
