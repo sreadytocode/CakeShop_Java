e2bb709f779a8f783e9ac1fdef003478fb858829
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-15 20:01:29 +0000

    Added to repositories

--- src/main/java/com/example/cakeShop/repositories/CakeRepository.java
+++ src/main/java/com/example/cakeShop/repositories/CakeRepository.java
@@ -4,6 +4,9 @@ import com.example.cakeShop.models.Cake;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CakeRepository extends JpaRepository<Cake, Long> {
+    List<Cake> findByName(String name);
 }
--- src/main/java/com/example/cakeShop/repositories/CakeShopRepository.java
+++ src/main/java/com/example/cakeShop/repositories/CakeShopRepository.java
@@ -4,6 +4,9 @@ import com.example.cakeShop.models.CakeShop;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CakeShopRepository extends JpaRepository<CakeShop, Long> {
+    List<CakeShop> findByName(String name);
 }
