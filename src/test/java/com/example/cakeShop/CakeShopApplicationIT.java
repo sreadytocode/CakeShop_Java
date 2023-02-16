a3cc3fc8a035de77a24967ed02903f1ddba05f65
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-16 15:47:44 +0000

    Wrote new integration test so it wouldn't interfere with postmapping integration test

--- src/test/java/com/example/cakeShop/CakeShopApplicationIT.java
+++ src/test/java/com/example/cakeShop/CakeShopApplicationIT.java
@@ -15,13 +15,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.lang.reflect.Field;
 import java.util.Arrays;
@@ -30,7 +30,11 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.Assert.*;
 import static org.springframework.http.ResponseEntity.status;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.web.servlet.function.RequestPredicates.contentType;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -46,54 +50,69 @@ public class CakeShopApplicationIT {
     @Autowired
     CakeRepository cakeRepository;
 
-
     @Test
-    public void getAllCakeShops() throws JsonProcessingException {
+    public void getAllCakeShops() {
 
-//      Arrange
-        CakeShop patisserieCakes = new CakeShop("Patisserie Cakes");
+//        Use ParametizedTypeReference to tell restTemplate that we expect a list of CakeShop objects as a response
+        ResponseEntity<List<CakeShop>> response = restTemplate.exchange("/cakeShops", HttpMethod.GET, null, new ParameterizedTypeReference<List<CakeShop>>() {});
 
-        Cake redVelvetCake = new Cake("Red Velvet Cake", 6.00, patisserieCakes);
-        redVelvetCake.setId(1L);
+        // Assert that the HTTP status code is 200 OK
+        assertEquals(HttpStatus.OK, response.getStatusCode());
 
-        Cake oreoCheeseCake = new Cake("Oreo Cheesecake", 6.00, patisserieCakes);
-        oreoCheeseCake.setId(2L);
+        // Assert that the response body is not null and contains at least one CakeShop object
+        List<CakeShop> cakeShops = response.getBody();
+        assertNotNull(cakeShops);
+        assertTrue(cakeShops.size() > 0);
 
-        Cake victoriaSponge = new Cake("Victoria Sponge Cake", 6.00, patisserieCakes);
-        victoriaSponge.setId(3L);
-
-        Cake blackForestCake = new Cake("Black Forest Cake", 5.90, patisserieCakes);
-        blackForestCake.setId(4L);
-
-        Cake tiramisu = new Cake("Tiramisu", 5.00, patisserieCakes);
-        tiramisu.setId(5L);
-
-        List <Cake> cakeList = Arrays.asList(redVelvetCake, oreoCheeseCake, victoriaSponge, blackForestCake, tiramisu);
-
-        patisserieCakes.setCakes(cakeList);
-        patisserieCakes.setId(1L);
-
-        CakeShop maya = new CakeShop("Maya desserts");
-        maya.setId(2L);
-
-        Cake tiramisuCake = new Cake("Tiramisu Cake", 32.95, maya);
-        tiramisuCake.setId(6L);
-        List <Cake> mayaCakeList = Arrays.asList(tiramisuCake);
-        maya.setCakes(mayaCakeList);
-
-        List<CakeShop> cakeShopList = Arrays.asList(patisserieCakes, maya);
-//        cakeShopRepository.saveAll(cakeShopList);
-//      Convert JSON into JSON string in this instance as easier to test full object as an arraylist
-        ObjectMapper objectMapper = new ObjectMapper();
-        String cakeShop = objectMapper.writeValueAsString(cakeShopList);
-
-//      Act
-        ResponseEntity<String> response = restTemplate.getForEntity("/cakeShops", String.class);
-
-//      Assert
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(response.getBody()).isEqualTo(cakeShop);
     }
+
+//    @Test
+//    public void getAllCakeShopsAndConvertToJsonStringFormat() throws JsonProcessingException {
+
+////      Arrange
+//        CakeShop patisserieCakes = new CakeShop("Patisserie Cakes");
+//
+//        Cake redVelvetCake = new Cake("Red Velvet Cake", 6.00, patisserieCakes);
+//        redVelvetCake.setId(1L);
+//
+//        Cake oreoCheeseCake = new Cake("Oreo Cheesecake", 6.00, patisserieCakes);
+//        oreoCheeseCake.setId(2L);
+//
+//        Cake victoriaSponge = new Cake("Victoria Sponge Cake", 6.00, patisserieCakes);
+//        victoriaSponge.setId(3L);
+//
+//        Cake blackForestCake = new Cake("Black Forest Cake", 5.90, patisserieCakes);
+//        blackForestCake.setId(4L);
+//
+//        Cake tiramisu = new Cake("Tiramisu", 5.00, patisserieCakes);
+//        tiramisu.setId(5L);
+//
+//        List <Cake> cakeList = Arrays.asList(redVelvetCake, oreoCheeseCake, victoriaSponge, blackForestCake, tiramisu);
+//
+//        patisserieCakes.setCakes(cakeList);
+//        patisserieCakes.setId(1L);
+//
+//        CakeShop maya = new CakeShop("Maya desserts");
+//        maya.setId(2L);
+//
+//        Cake tiramisuCake = new Cake("Tiramisu Cake", 32.95, maya);
+//        tiramisuCake.setId(6L);
+//        List <Cake> mayaCakeList = Arrays.asList(tiramisuCake);
+//        maya.setCakes(mayaCakeList);
+//
+//        List<CakeShop> cakeShopList = Arrays.asList(patisserieCakes, maya);
+////        cakeShopRepository.saveAll(cakeShopList);
+////      Convert JSON into JSON string in this instance as easier to test full object as an arraylist
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String cakeShop = objectMapper.writeValueAsString(cakeShopList);
+//
+////      Act
+//        ResponseEntity<String> response = restTemplate.getForEntity("/cakeShops", String.class);
+//
+////      Assert
+//        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+//        assertThat(response.getBody()).isEqualTo(cakeShop);
+//    }
 
     @Test
     public void getCakeShop(){
@@ -105,6 +124,21 @@ public class CakeShopApplicationIT {
         assertThat(response.getBody().getId()).isEqualTo(patisserieCakes.getId());
         assertThat(response.getBody().getName()).isEqualTo(patisserieCakes.getName());
         assertThat(response.getBody().getCakes()).isEqualTo(patisserieCakes.getCakes());
+    }
+
+    @Test
+    public void testPostCakeShops() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        CakeShop newCakeShop = new CakeShop("New CakeShop");
+
+        HttpEntity<CakeShop> request = new HttpEntity<>(newCakeShop, headers);
+        ResponseEntity<CakeShop> response = restTemplate.postForEntity("/cakeShops", request, CakeShop.class);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getBody().getId());
+
     }
 
 
