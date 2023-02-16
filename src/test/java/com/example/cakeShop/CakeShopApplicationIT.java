08f72db39f647ed912125776ffe7e9c725916f55
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-16 13:38:05 +0000

    Wrote test for get mapping so now tests get fetch call and passes

--- src/test/java/com/example/cakeShop/CakeShopApplicationIT.java
+++ src/test/java/com/example/cakeShop/CakeShopApplicationIT.java
@@ -4,12 +4,17 @@ import com.example.cakeShop.models.Cake;
 import com.example.cakeShop.models.CakeShop;
 import com.example.cakeShop.repositories.CakeRepository;
 import com.example.cakeShop.repositories.CakeShopRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -18,8 +23,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.springframework.http.ResponseEntity.status;
@@ -39,11 +47,36 @@ public class CakeShopApplicationIT {
     CakeRepository cakeRepository;
 
     @Test
-    public void getAllCakeShops() {
-        ResponseEntity<List> response = restTemplate.getForEntity("/cakeShops", List.class);
+    public void getAllCakeShops() throws JsonProcessingException {
+
+//        Arrange
+        CakeShop patisserieCakes = new CakeShop("Patisserie Cakes");
+
+        Cake redVelvetCake = new Cake("Red Velvet Cake", 6.00, patisserieCakes);
+        redVelvetCake.setId(1L);
+        Cake oreoCheeseCake = new Cake("Oreo Cheesecake", 6.00, patisserieCakes);
+        oreoCheeseCake.setId(2L);
+        Cake victoriaSponge = new Cake("Victoria Sponge Cake", 6.00, patisserieCakes);
+        victoriaSponge.setId(3L);
+        Cake blackForestCake = new Cake("Black Forest Cake", 5.90, patisserieCakes);
+        blackForestCake.setId(4L);
+        Cake tiramisu = new Cake("Tiramisu", 5.00, patisserieCakes);
+        tiramisu.setId(5L);
+        List <Cake> cakeList = Arrays.asList(redVelvetCake, oreoCheeseCake, victoriaSponge, blackForestCake, tiramisu);
+        patisserieCakes.setCakes(cakeList);
+        patisserieCakes.setId(1L);
+        List<CakeShop> cakeShopList = Arrays.asList(patisserieCakes);
+
+//      Convert JSON into JSON string
+        ObjectMapper objectMapper = new ObjectMapper();
+        String cakes = objectMapper.writeValueAsString(cakeShopList);
+
+//      Act
+        ResponseEntity<String> response = restTemplate.getForEntity("/cakeShops", String.class, patisserieCakes.getId());
+
+//      Assert
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-//        Does get request and returns object but expected needs to be amended hence commented out for now
-//        assertThat(response.getBody()).isEqualTo(cakeShopRepository.findAll());
+        assertThat(response.getBody()).isEqualTo(cakes);
     }
 
     @Test
@@ -58,4 +91,6 @@ public class CakeShopApplicationIT {
         assertThat(response.getBody().getName()).isEqualTo(patisserieCakes.getName());
         assertThat(response.getBody().getCakes()).isEqualTo(patisserieCakes.getCakes());
     }
+
+
 }
